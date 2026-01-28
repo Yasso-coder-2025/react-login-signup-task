687df3a102d4403119cfc54b29bc1d2238558492
Yassin <yassenhisham775@gmail.com>
2026-01-28 15:30:14 +0200

    connect the frontEnd with backEnd and add validation for login and signup

--- src/components/Section/Login.jsx
+++ src/components/Section/Login.jsx
@@ -1,10 +1,11 @@
-import { Formik } from 'formik'
+import { ErrorMessage, Field, Form, Formik } from 'formik'
 import React from 'react'
 import { facebookIcon, googleIcon } from '../../assets/icon/icons'
 import Header from '../Header/Footer/Header'
 import Footer from '../Header/Footer/Footer'
 import { Link } from 'react-router'
 import axios from 'axios'
+import * as Yup from 'yup';
 
 // async function loginData() {
 //   const res = await fetch('https://bookstore.eraasoft.pro/api/login', {
@@ -33,60 +34,83 @@ import axios from 'axios'
 
 //axios  with ai helpied
  
-async function loginData() {
-  const res = await axios.post(
-    "https://bookstore.eraasoft.pro/api/login",
-    {
-      email: "mostafa@gmail.com",
-      password: "password",
-    },
-    {
-      headers: {
-        Accept: "application/json",
-      },
-    }
-  );
+// async function loginData() {
+//   const res = await axios.post(
+//     "https://bookstore.eraasoft.pro/api/login",
+//     {
+//       email: "mostafa@gmail.com",
+//       password: "password",
+//     },
+//     {
+//       headers: {
+//         Accept: "application/json",
+//       },
+//     }
+//   );
 
-  return res.data;
-}
+//   return res.data;
+// }
 
-loginData()
-  .then(data => {
-    console.log(data);
-  })
-  .catch(err => {
-    console.log(err.response.data);
-  });
+// loginData()
+//   .then(data => {
+//     console.log(data);
+//   })
+//   .catch(err => {
+//     console.log(err.response.data);
+//   });
+
+// ignore what above just for learning axios and fetch
+
+
+
 
 
 
 const Login = () => {
-    
+
+    const handleLogin = async (values) => {
+        console.log(values);
+        try {
+          const res = await axios.post ("https://bookstore.eraasoft.pro/api/login", values);
+          console.log(res);
+        } catch (error) {
+          console.log(error);
+        }
+
+    }
+
+    const loginSchema = Yup.object({
+      email: Yup.string("Enter invalid email").email("Enter a valid email").required("Hey! your Email isn't written"),
+      password: Yup.string().min(7, "Password must be at least 7 characters").required("Hey!!! your password isn't written also"), 
+    })
+
 
   return (
     <>
     <Header/>
     <div className="flex flex-col justify-center items-center gap-10 mt-15">
         <h4 className='text-(--mainColor) font-semibold text-lg'>Welcome Back!</h4>
-        <Formik>
-            <form className='w-xl'>
+        <Formik initialValues={{email: "", password: "" }} validationSchema={loginSchema} onSubmit={ (values) => handleLogin(values)}>
+            <Form className='w-xl'>
                 <div className="flex flex-col gap-6">
                     <div className="flex flex-col gap-2">
                         <label htmlFor="email" className='text-[18px] font-semibold'>Email</label>
-                        <input type="gmail"  placeholder='example@gmail.com' className=' border border-black/20 rounded-lg outline-none w-full p-4'/>
+                        <Field name="email" type="text"  placeholder='example@gmail.com' className=' border border-black/20 rounded-lg outline-none w-full p-4'/>
                     </div>
+                    <ErrorMessage name="email" component={"p"} className="text-red-500 text-sm" />
                     <div className="flex flex-col gap-2">
                         <label htmlFor="password" className='text-[18px] font-semibold'>Password</label>
-                        <input type="password"  placeholder='Enter password' className=' border border-black/20 rounded-lg outline-none w-full p-4'/>
+                        <Field name="password" type="password"  placeholder='Enter password' className=' border border-black/20 rounded-lg outline-none w-full p-4'/>
                     </div>
+                    <ErrorMessage name="password" component={"p"} className="text-red-500 text-sm" />
                     <div className="flex justify-between items-center">
                         <div className="flex items-center gap-0.75">
-                            <input type='checkbox' name='checkbox'/>
+                            <Field  type='checkbox' name='checkbox'/>
                             <label htmlFor="checkbox" className='font-normal text-[14px]'>Remember me</label>
                         </div>
                         <a className="text-[18px] font-normal text-(--mainColor) cursor-pointer"> Forget password?</a>
                     </div>
-                        <button className="mt-4 px-4 py-2.5 bg-(--mainColor) text-white rounded-lg cursor-pointer border border-(--mainColor) hover:bg-white hover:text-(--mainColor)  duration-300">
+                        <button type="submit" className="mt-4 px-4 py-2.5 bg-(--mainColor) text-white rounded-lg cursor-pointer border border-(--mainColor) hover:bg-white hover:text-(--mainColor)  duration-300">
                         Log in
                         </button>
                 </div>
@@ -108,7 +132,7 @@ const Login = () => {
                         Login with Facebook
                     </button>
                 </div>
-            </form>
+            </Form>
         </Formik>
     </div>
     <Footer/>
--- src/components/Section/SignUp.jsx
+++ src/components/Section/SignUp.jsx
@@ -1,46 +1,81 @@
-import { Formik } from 'formik'
-import React from 'react'
+import { ErrorMessage, Field, Form, Formik } from 'formik'
+import React, { use } from 'react'
 import { facebookIcon, googleIcon } from '../../assets/icon/icons'
 import Header from '../Header/Footer/Header'
 import Footer from '../Header/Footer/Footer'
 import { Link } from 'react-router'
+import * as Yup from 'yup'
+import axios from 'axios'
+import { useNavigate } from 'react-router'
 
-const SignUp = () => {
+
+
+const SignUp = () => { 
+    
+    const navigate = useNavigate();
+    
+    
+    const handleSignUp = async (values)=>{
+        try {
+            const res = await axios.post("https://bookstore.eraasoft.pro/api/register", values);
+            navigate('/');
+            console.log(res);
+        } catch (error) {
+            console.log(error);
+        }
+    }
+
+    const SignUpSchema = Yup.object({
+        first_name: Yup.string().required("First name is required"),
+        last_name: Yup.string().required("Last name is required"),
+        email: Yup.string().email("Invalid email address").required("Email is required"),
+        password: Yup.string().required("Password is required"),
+        password_confirmation : Yup.string().oneOf([Yup.ref("password")], "Passwords must match").required("Confirm password is required")
+    })
+
+ 
+
   return (
     <>
     <Header/>
     <div className="flex flex-col justify-center items-center gap-10 mt-15">
-        <Formik>
-            <form className='w-xl'>
+        <Formik initialValues={{first_name: "", last_name: "", email: "", password: "", confirm_password: "" }} validationSchema={SignUpSchema} onSubmit={ (values) =>{handleSignUp(values)}}>
+            <Form className='w-xl'>
                 <div className="user-name grid grid-cols-2 gap-4 mb-6">
                 <div className="flex flex-col gap-2">
                     <label htmlFor="text" className='text-[18px] font-semibold'>First Name</label>
-                    <input type="text"  placeholder='John' className=' border border-black/20 rounded-lg outline-none w-full p-4'/>
+                    <Field  name="first_name" type="text"  placeholder='John' className=' border border-black/20 rounded-lg outline-none w-full p-4'/>
+                    <ErrorMessage name="first_name" component="div" className="text-red-500 text-sm" />
                 </div>
                 <div className="flex flex-col gap-2">
                     <label htmlFor="text" className='text-[18px] font-semibold'>last Name</label>
-                    <input type="text"  placeholder='Smith' className=' border border-black/20 rounded-lg outline-none w-full p-4'/>
+                    <Field name="last_name" type="text"  placeholder='Smith' className=' border border-black/20 rounded-lg outline-none w-full p-4'/>
+                    <ErrorMessage name="last_name" component="div" className="text-red-500 text-sm" />
                 </div>
-
                 </div>
                 <div className="flex flex-col gap-6">
                     <div className="flex flex-col gap-2">
                         <label htmlFor="email" className='text-[18px] font-semibold'>Email</label>
-                        <input type="gmail"  placeholder='example@gmail.com' className=' border border-black/20 rounded-lg outline-none w-full p-4'/>
+                        <Field  name="email" type="email"  placeholder='example@gmail.com' className=' border border-black/20 rounded-lg outline-none w-full p-4'/>
+                    <ErrorMessage name="email" component="div" className="text-red-500 text-sm" />
                     </div>
                     <div className="flex flex-col gap-2">
                         <label htmlFor="password" className='text-[18px] font-semibold'>Password</label>
-                        <input type="password"  placeholder='Enter password' className=' border border-black/20 rounded-lg outline-none w-full p-4'/>
+                        <Field name="password" type="password"  placeholder='Enter password' className=' border border-black/20 rounded-lg outline-none w-full p-4'/>
+                    <ErrorMessage name="password" component="div" className="text-red-500 text-sm" />
+
                     </div>
                     <div className="flex flex-col gap-2">
                         <label htmlFor="password" className='text-[18px] font-semibold'>Confirm Password</label>
-                        <input type="password"  placeholder='Enter password' className=' border border-black/20 rounded-lg outline-none w-full p-4'/>
+                        <Field name="password_confirmation" type="password"  placeholder='Enter password' className=' border border-black/20 rounded-lg outline-none w-full p-4'/>
+                    <ErrorMessage name="password_confirmation" component="div" className="text-red-500 text-sm" />
+
                     </div>
                     <div className="flex items-center gap-0.75">
-                        <input type='checkbox' name='checkbox'/>
+                        <Field type='checkbox' name='checkbox'/>
                         <label htmlFor="checkbox" className='font-normal text-[14px] pl-2'>Agree with <a className='text-(--mainColor) cursor-pointer'>Terms & Conditions</a></label>
                     </div>
-                        <button className="mt-4 px-4 py-2.5 bg-(--mainColor) text-white rounded-lg cursor-pointer border border-(--mainColor) hover:bg-white hover:text-(--mainColor)  duration-300">
+                        <button type="submit" className="mt-4 px-4 py-2.5 bg-(--mainColor) text-white rounded-lg cursor-pointer border border-(--mainColor) hover:bg-white hover:text-(--mainColor)  duration-300">
                         Sign Up
                         </button>
                 </div>
@@ -62,7 +97,7 @@ const SignUp = () => {
                         Login with Facebook
                     </button>
                 </div>
-            </form>
+            </Form>
         </Formik>
     </div>
     <Footer/>
